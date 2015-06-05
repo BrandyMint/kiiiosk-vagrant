e29214c6869c74b50173c0c5ed0e5c716e2428f3
Denis Khabarov <admin@saymon21-root.pro>
2015-06-05 14:52:36 +0300

    Старт elasticsearch

--- provisions/elastic.sh
+++ provisions/elastic.sh
@@ -21,7 +21,7 @@ wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-k
 add-apt-repository -y 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main';
 apt-get update && apt-get -y install elasticsearch
 update-rc.d elasticsearch defaults 95 10;
-
+service elasticsearch start
 # Попробую пока без него, вроде должно и так работать.
 #/usr/share/elasticsearch/bin/plugin -l|grep analysis-morphology || /usr/share/elasticsearch/bin/plugin -install analysis-morphology -url http://dl.bintray.com/content/imotov/elasticsearch-plugins/org/elasticsearch/elasticsearch-analysis-morphology/1.2.0/elasticsearch-analysis-morphology-1.2.0.zip;
 
